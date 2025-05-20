37bba57485afbe403b7ee98ed9742a0351ea4417
Hanna Hermsen <hanna.hermsen@ns.nl>
2025-05-20 11:02:28 +0200

    refactor: identify code smells with code comments (end of pomodoro 4)

--- src/main/kotlin/Rover.kt
+++ src/main/kotlin/Rover.kt
@@ -1,21 +1,26 @@
 package org.example
 
 class Rover {
-    constructor(p: String) {
-        val s = p.split(' ')
+    constructor(p: String) { // Unclear naming
+        val s = p.split(' ') // Unclear naming
         if (s.size >= 3) {
+            // primitive obsession, feature envy, data clump?,
             rs.xx = s[0].toInt()
             rs.yy = s[1].toInt()
             rs.dd = s[2][0]
         }
     }
 
-    fun go(cms: String) {
-        for (c in cms) {
-            when (c) {
-                'L' -> {
-                    when (rs.dd) {
-                        'E' -> rs.dd = 'N' 'N' -> rs.dd = 'W' 'W' -> rs.dd = 'S' 'S' -> rs.dd = 'E'
+    fun go(cms: String) { // unclear naming (cms and go),
+        // primitive obsession (not a string but a list of enum values representing the commands)
+        for (c in cms) { // Unclear naming
+            when (c) { // switch statement
+                'L' -> { // primitive obsession, unclear naming
+                    when (rs.dd) { // too many levels of indentation, bad indentation, switch statement, feature envy
+                        'E' -> rs.dd = 'N'
+                        'N' -> rs.dd = 'W'
+                        'W' -> rs.dd = 'S'
+                        'S' -> rs.dd = 'E'
                     }
                 }
                 'R' -> {
@@ -36,6 +41,7 @@ class Rover {
         go(z.toString())
     }
 
+    // unclear naming, feature envy
     val xyd: String
         get() = "${rs.xx} ${rs.yy} ${rs.dd}"
 
@@ -43,11 +49,11 @@ class Rover {
 
     constructor() : this("")
 
-    private var rs = RoverState()
+    private var rs = RoverState() // Unclear naming
 }
 
 class RoverState {
-    var xx: Int = 0
-    var yy: Int = 0
-    var dd: Char = 'N'
+    var xx: Int = 0 // Unclear naming
+    var yy: Int = 0 // Unclear naming
+    var dd: Char = 'N' // Unclear naming
 }
