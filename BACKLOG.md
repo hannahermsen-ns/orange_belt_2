f188bfb5606affddc742478dda596a655a12dc2d
Hanna Hermsen <hanna.hermsen@ns.nl>
2025-05-20 11:49:32 +0200

    refactor: extract functions for handling each possible direction

--- BACKLOG.md
+++ BACKLOG.md
@@ -16,5 +16,7 @@ Code smell solving plan:
    7. Rename xyd instance variable X
    8. Rename rs variable X
    9. Rename members of RoverState X
-2. Reduce size of the go method (reduce complexity, as per the refactoring priority premise)
+2. Reduce size of the executeInstructions method (reduce complexity, as per the refactoring priority premise)
+   3. Extract handling instructions for each possible direction X
+   4. Extract logic that handles an instruction to a separate function
 3. Reduce duplication of switch statements checking the commands (reduce complexity, as per the refactoring priority premise)
--- src/main/kotlin/Rover.kt
+++ src/main/kotlin/Rover.kt
@@ -11,30 +11,48 @@ class Rover {
         }
     }
 
-    fun executeInstructions(instructions: String) { // long method
+    fun executeInstructions(instructions: String) {
         // primitive obsession (not a string but a list of enum values representing the commands)
         for (instruction in instructions) {
             when (instruction) { // switch statement
                 'L' -> { // primitive obsession
-                    when (roverState.orientation) { // too many levels of indentation, bad indentation, switch statement, feature envy
-                        'E' -> roverState.orientation = 'N'
-                        'N' -> roverState.orientation = 'W'
-                        'W' -> roverState.orientation = 'S'
-                        'S' -> roverState.orientation = 'E'
-                    }
+                    handleLeftInstruction()
                 }
                 'R' -> {
-                    when (roverState.orientation) {
-                        // using the same when in multiple places (duplicated code)
-                        'E' -> roverState.orientation = 'S' 'S' -> roverState.orientation = 'W' 'W' -> roverState.orientation = 'N' 'N' -> roverState.orientation = 'E'
-                    }
+                    handleRightInstruction()
                 }
                 'M' -> {
-                    when (roverState.orientation) {
-                        'E' -> roverState.x++ 'S' -> roverState.y-- 'W' -> roverState.x-- 'N' -> roverState.y++
-                    }
+                    handleMoveInstruction()
                 }
             }
+        }
+    }
+
+    private fun handleMoveInstruction() {
+        when (roverState.orientation) {
+            'E' -> roverState.x++
+            'S' -> roverState.y--
+            'W' -> roverState.x--
+            'N' -> roverState.y++
+        }
+    }
+
+    private fun handleRightInstruction() {
+        when (roverState.orientation) {
+            // using the same when in multiple places (duplicated code)
+            'E' -> roverState.orientation = 'S'
+            'S' -> roverState.orientation = 'W'
+            'W' -> roverState.orientation = 'N'
+            'N' -> roverState.orientation = 'E'
+        }
+    }
+
+    private fun handleLeftInstruction() {
+        when (roverState.orientation) { // too many levels of indentation, bad indentation, switch statement, feature envy
+            'E' -> roverState.orientation = 'N'
+            'N' -> roverState.orientation = 'W'
+            'W' -> roverState.orientation = 'S'
+            'S' -> roverState.orientation = 'E'
         }
     }
 
